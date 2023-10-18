521c71d46854c6a9e2493db8c096f2cc63c0cee6
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-18 15:08:45 +0330

    fixed a bug in last commit

--- component/section/home/secondSectionComponent.tsx
+++ component/section/home/secondSectionComponent.tsx
@@ -32,54 +32,54 @@ export default function SecondSectionComponent():ReactNode {
                     <h1 className={`sm:text-center lg:text-[50px] text-[40px] text-gray-800 font-bold lg:leading-[60px] leading-[40px] mb-[30px] ${walsheimFont.className}`}>A superpowered <br/> meeting in every <br/> calendar event</h1>
                     <p className={'sm:text-center text-gray-800 text-[18px] font-normal lg:mb-[120px] mb-[80px]'}>Calendar events in Ready are instantly transformed into <br/> collaborative meeting spaces designed to create the <br/> perfect meeting in seconds.</p>
                     <div id={'second-section-image-holder'} className={'flex gap-[20px] -translate-x-[50%]'}>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
-                        <img className={'lg:w-[30vw] w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage1.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage2.src} alt="Demo image of the ready app"/>
+                        <img className={'lg:w-[30vw] object-cover shrink-0 w-[20rem]'} src={demoImage3.src} alt="Demo image of the ready app"/>
                     </div>
                 </HolderComponent>
             </main>
