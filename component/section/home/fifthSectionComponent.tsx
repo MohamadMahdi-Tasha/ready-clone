5e161a0afd3ba6924a42a67627d515abd6b12822
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-19 11:52:01 +0330

    added styles and structures of last commit

--- component/section/home/fifthSectionComponent.tsx
+++ component/section/home/fifthSectionComponent.tsx
@@ -9,22 +9,22 @@ import demoImage from '@/public/img/section/fifthSection/img-demo.webp';
 export default function FifthSectionComponent():ReactNode {
     // Returning JSX
     return (
-        <section className={'mt-[50px]'}>
+        <section className={'mt-[50px] bg-gradient-to-b from-transparent to-indigo-200'}>
             <main>
-                <HolderComponent>
-                    <div>
-                        <h1 className={`sm:text-center lg:text-[50px] text-[40px] text-gray-800 font-bold lg:leading-[60px] leading-[40px] mb-[30px] ${walsheimFont.className}`}><span>All</span> in a calendar, <br/> that’s a <span>joy to use</span></h1>
-                        <p className={'sm:text-center text-gray-800 text-[18px] font-normal lg:mb-[120px] mb-[80px]'}>An entire meeting collaboration platform built inside a fast <br/> and modern calendar you and your team will love.</p>
-                        <div>
-                            <div>Desktop </div>
-                            <div>AppKeyboard </div>
-                            <div>Shortcuts</div>
-                            <div>Dark & Light Mode</div>
-                            <div>Google Calendar Sync</div>
-                            <div>Notifications</div>
+                <HolderComponent className={'px-[20px]'}>
+                    <div className={'mb-[100px]'}>
+                        <h1 className={`sm:text-center lg:text-[50px] text-[40px] text-gray-800 font-bold lg:leading-[60px] leading-[40px] mb-[30px] ${walsheimFont.className}`}><span className={'bg-line-fifth-section-1'}>All</span> in a calendar, <br/> that’s a <span className={'bg-line-fifth-section-2'}>joy to use</span></h1>
+                        <p className={'sm:text-center text-gray-800 text-[18px] font-normal mb-[50px]'}>An entire meeting collaboration platform built inside a fast <br/> and modern calendar you and your team will love.</p>
+                        <div className={'flex flex-wrap items-center justify-center mx-auto gap-[10px] lg:w-[40%]'}>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>Desktop </div>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>App Keyboard </div>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>Shortcuts</div>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>Dark & Light Mode</div>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>Google Calendar Sync</div>
+                            <div className={'px-[20px] py-[10px] text-[15px] rounded-[50rem] font-bold text-indigo-500 border border-indigo-200'}>Notifications</div>
                         </div>
                     </div>
-                    <img src={demoImage.src} alt="Ready App"/>
+                    <img className={'w-full'} src={demoImage.src} alt="Ready App"/>
                 </HolderComponent>
             </main>
         </section>
